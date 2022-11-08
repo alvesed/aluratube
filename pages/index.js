22ba27a1addb1f6bf31a1f1a4281b48a92bee8e6
Edson Francisco Alves <alvesed@gmail.com>
2022-11-08 19:53:17 -0300

    atualizacao aula 2

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,14 @@ import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
+import { StyledFavorites } from "../src/components/Favorites";
+import React from "react";
 
 function HomePage() {
   //const estiloHomePage = { backgroundColor: "red" };
 
   //console.log(config.playlists);
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
   return (
 
@@ -16,14 +19,17 @@ function HomePage() {
       <div style={{
         display: "flex",
         flexDirection: "column",
-        flex: 1,
+        flex: 1
         // backgroundColor: "red",
       }}>
-        <Menu />
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
         <Header />
-        <Timeline playlists={config.playlists}>
+        <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
           Conteúdo
         </Timeline>
+        <Favoritos favorites={config.favorites}>
+          Conteúdo
+        </Favoritos>
       </div>
     </>
   );
@@ -40,7 +46,6 @@ const StyledHeader = styled.div`
     border-radius: 50%;
   }
   .user-info {
-    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -48,11 +53,18 @@ const StyledHeader = styled.div`
     gap: 16px;
   }
 `;
+
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    /* background-image: url(${config.bg}); */
+    height: 230px;
+`;
+
 function Header() {
   return (
     <StyledHeader>
-      <div></div>
-      {/*    <img src="banner" /> */}
+      <StyledBanner bg={config.bg} />
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
@@ -68,7 +80,7 @@ function Header() {
   )
 }
 
-function Timeline(props) {
+function Timeline({searchValue, ...props}) {
   //console.log("Dentro do componente", props.playlists);
   const playlistNames = Object.keys(props.playlists);
 
@@ -78,26 +90,61 @@ function Timeline(props) {
     <StyledTimeline>
       {playlistNames.map((playlistName) => {
         const videos = props.playlists[playlistName];
-        console.log(playlistName);
-        console.log(videos);
+        //console.log(playlistName);
+        //console.log(videos);
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>
-                      {video.title}
-                    </span>
-                  </a>
-                )
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
+                  return titleNormalized.includes(searchValueNormalized)
+                })
+                .map((video) => {
+                  return (
+                    <a key={video.url} href={video.url}>
+                      <img src={video.thumb} />
+                      <span>
+                        {video.title}
+                      </span>
+                    </a>
+                  )
               })}
             </div>
           </section>
         )
       })}
     </StyledTimeline>
+  )
+}
+
+function Favoritos(props) {
+  //console.log("CONTEÚDO listFavorites");
+  const listFavorites = props.favorites;
+  //console.log(listFavorites);
+
+  //console.log("listFavorites map");
+  //listFavorites.map((favorite) => {
+  //  console.log(favorite);
+  //});
+
+  return (
+    <StyledFavorites>
+        <section>
+          <h2>Favoritos</h2>
+          <div>
+            {listFavorites.map((favorite) => {
+                //console.log(favorite.urlfav);
+                return (
+                  <a key={favorite.urlfav} href={favorite.urlfav}>
+                    <img src={favorite.img} />
+                  </a>
+                )
+            })}
+          </div>
+        </section>
+    </StyledFavorites>
   )
 }
